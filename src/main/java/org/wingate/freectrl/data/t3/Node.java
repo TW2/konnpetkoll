d961e1b037f91cf7c385bd38267116464d8e4798
TW2 <util2@msn.com>
2021-03-07 20:06:36 +0100

    Free controls

---
+++ src/main/java/org/wingate/freectrl/data/t3/Node.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.wingate.freectrl.data.t3;
+
+/**
+ * This interface is a declarative object of Node
+ * @author util2
+ */
+public interface Node {
+    /**
+     * Get the name of the Node
+     * @return A name
+     */
+    public String getName();
+    /**
+     * Set the name of the Node
+     * @param name A name
+     */
+    public void setName(String name);
+}
